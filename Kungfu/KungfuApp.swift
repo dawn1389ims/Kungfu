11c2f6bf00e916fd6553aa212da1713f9233f938
ZhuYeQing <zzq1389xin@163.com>
2023-05-08 19:24:04 +0800

    Initial Commit

---
+++ Kungfu/KungfuApp.swift
@@ -0,0 +1,17 @@
+//
+//  KungfuApp.swift
+//  Kungfu
+//
+//  Created by zhiqiang zhu on 2023/5/8.
+//
+
+import SwiftUI
+
+@main
+struct KungfuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
